228358aaed0a47727ae176c4d78f576630f2ae41
jvses <180137221@aluno.unb.br>
2023-07-05 12:36:31 -0300

    acréscimo do segundo sensor de linha

--- sensores/seguidor_linha.py
+++ sensores/seguidor_linha.py
@@ -3,16 +3,19 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-sensor = 17
+sensor_left = 17
+sensor_right = 27
 
 GPIO.setup(sensor, GPIO.IN)
 
 try:
 	while True:
-		if not GPIO.input(sensor):
-			print("Saiu da linha preta!")
-		else:
-			print("Following the line!: está no pretp")
+        if not GPIO.input(sensor_left):
+            print("Robot is straying off to the right, move left captain!")
+        elif not GPIO.input(sensor_right):
+            print("Robot is straying off to the left, move right captain!")
+        else:
+            print("Following the line!")
 		sleep(0.2)
 except:
 	GPIO.cleanup()
