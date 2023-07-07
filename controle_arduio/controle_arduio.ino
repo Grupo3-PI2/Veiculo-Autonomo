1f098ec1e2453aa1b0882e6df7cf462f4ca6d5f4
jvses <180137221@aluno.unb.br>
2023-07-06 22:40:17 -0300

    problemas no ultrasonico

--- controle_arduio/controle_arduio.ino
+++ controle_arduio/controle_arduio.ino
@@ -1,33 +1,44 @@
-#include <HCSR04.h>
+//#include <HCSR04.h>
 #include <Wire.h>
+#include "Ultrasonic.h"
 
-#define ENA 13 /* Enable ativa os drivers do motor */
-#define MS1 12 /* Seleciona a resolução do passo */
-#define MS2 11 /* Seleciona a resolução do passo   */
-#define MS3 10 /* Seleciona a resolução do passo */
-#define RST 9 /* Desativa os drivers de saída   */
-#define SLP 8 /* Inicia um descanso para algumas funções do drive */
-#define DIR 7 /* Direção do passo  */
-#define STP 6 /* Avanço do passo */
-#define TRIGGER_SL   5
-#define ECHO_SL      4
-#define TRIGGER_SR   3
-#define ECHO_SR      2
-#define linha_L      1
-#define linha_R      0
+#define ENA 7 /* Enable ativa os drivers do motor */
+#define MS1 6 /* Seleciona a resolução do passo */
+#define MS2 4 /* Seleciona a resolução do passo   */
+#define MS3 3 /* Seleciona a resolução do passo */
+//#define RST 9 /* Desativa os drivers de saída   */
+//#define SLP 8 /* Inicia um descanso para algumas funções do drive */
+#define DIR 2 /* Direção do passo  */
+#define STP 5 /* Avanço do passo */
+#define TRIGGER_SL   11
+#define ECHO_SL      10
+#define TRIGGER_SR   13
+#define ECHO_SR      12
+#define linha_L      9
+#define linha_R      8
+#define motPin1 14
+#define motPin2 15
+
 //#define max_passo 500 /* pulsos por resolução */
 #define temp 1000
 
 bool sen_linha_L = 0;
 bool sen_linha_R = 0;
+int dist = 0;
+int distancia = 0;
 
 /*---- Variáveis de controle motor de passo---- */
 int passo = 0;    /* passos */
 
+String result; //VARIÁVEL DO TIPO STRING
+String result_2; //VARIÁVEL DO TIPO STRING
 
-
+void print_speed_dist();
+void esquerda();
+void direita();
 // Inicializa o sensor usando os pinos TRIGGER and ECHO.
-UltraSonicDistanceSensor sen_dist_L(TRIGGER_SL, ECHO_SL), sen_dist_R(TRIGGER_SR, ECHO_SR);
+//UltraSonicDistanceSensor sen_dist_L(TRIGGER_SL, ECHO_SL), sen_dist_R(TRIGGER_SR, ECHO_SR);
+Ultrasonic sen_distL(11, 10), sen_distR(13, 12);
 
 void mpu_begin();
 void motoPassoBegin();
@@ -39,18 +50,53 @@ const int MPU = 0x68;
 //Variaveis para armazenar valores dos sensores
 int AcX, AcY, AcZ, GyX, GyY, GyZ;
 
+int v = 0; // VARIÁVEL DA VELOCIDADE DO CARRINHO
+
+// Iremos fazer uma classe para facilitar o uso da ponte H L298N na manipulação dos motores na função Setup e Loop.
+class DCMotor {
+    int spd = 255, pin1, pin2;
+
+  public:
+
+    void Pinout(int in1, int in2) { // Pinout é o método para a declaração dos pinos que vão controlar o objeto motor
+      pin1 = in1;
+      pin2 = in2;
+      pinMode(pin1, OUTPUT);
+      pinMode(pin2, OUTPUT);
+    }
+    void Speed(int in1) { // Speed é o método que irá ser responsável por salvar a velocidade de atuação do motor
+      spd = in1;
+    }
+    void Forward() { // Forward é o método para fazer o motor girar para frente
+      analogWrite(pin1, spd);
+      digitalWrite(pin2, LOW);
+    }
+    void Backward() { // Backward é o método para fazer o motor girar para trás
+      digitalWrite(pin1, LOW);
+      analogWrite(pin2, spd);
+    }
+    void Stop() { // Stop é o metodo para fazer o motor ficar parado.
+      digitalWrite(pin1, LOW);
+      digitalWrite(pin2, LOW);
+    }
+};
+DCMotor Motor2; // Criação de dois objetos motores, já que usaremos dois motores, e eles já estão prontos para receber os comandos já configurados acima.
 void setup() {
   //  motoPassoBegin();
   //Setamos os pinos dos seguidores de linha como entrada
   pinMode(linha_L, INPUT);
   pinMode(linha_R, INPUT);
+  ///////////pinagem do motor de passo
+  motoPassoBegin();
 
+  //begin do motor DC
+  Motor2.Pinout(motPin1, motPin2);
   Serial.begin(9600);
   while (!Serial)
     ;
   Serial.print(F("Comunicacao serial estabelecida \nInicializando componentes"));
   ///MPU begin
-  mpu_begin();
+//  mpu_begin();
   // sensor de distância não precisa de inicialização
 
   Serial.println(F("..INIT.."));
@@ -61,10 +107,11 @@ void loop() {
 
   //////////////////////////////////////////////////
   // MPU
-  getMpuData();
+  //  getMpuData();
   ////////////////////////////////////////////////////////////
   // sensores de Distancia
-  pegar_distancias();
+//  pegar_distancias();
+
   // Seguidor de linha
   sen_linha_L = digitalRead(linha_L);
   sen_linha_R = digitalRead(linha_R);
@@ -77,25 +124,67 @@ void loop() {
     direita();
   }
   else {
-  Serial.println(F("Estou seguindo a linha"));
+    Serial.println(F("Estou seguindo a linha"));
+  }
+  if (distancia < 70 || dist < 70 ) {
+    Motor2.Stop();
+    delay(10);
+    print_speed_dist();
+  }
+  else if (distancia < 90 || dist < 90 ) {
+    Motor2.Speed(80);// A velocidade do motor pode variar de 64 a 255, onde 255 é a velocidade máxima e 64 o mínimo.
+    Motor2.Backward();
+    print_speed_dist();
+    delay(10);
+  }
+  else if (distancia < 110 || dist < 110) {
+    Motor2.Speed(100);// A velocidade do motor pode variar de 64 a 255, onde 255 é a velocidade máxima e 64 o mínimo.
+    Motor2.Backward();
+    print_speed_dist();
+    delay(10);
   }
 
+  else if (distancia < 130 || dist < 130) {
+    Motor2.Speed(130);// A velocidade do motor pode variar de 64 a 255, onde 255 é a velocidade máxima e 64 o mínimo.
+    Motor2.Backward();
+    print_speed_dist();
+    delay(10);
+  }
+  else {
+    Motor2.Speed(160);// A velocidade do motor pode variar de 64 a 255, onde 255 é a velocidade máxima e 64 o mínimo.
+    Motor2.Backward();
+    print_speed_dist();
+    delay(10);
+  }
+}
 
-  //  delay(500);
-
+void print_speed_dist() {
+  Serial.println(dist);
+  Serial.println(distancia);
+  Serial.println(v);
 
 }
+
+//  delay(500);
+
+
+
 ////Valores distâncias////////////////////////////////////////////////////////
 void pegar_distancias() {
   //Cria variavel do tipo int
-  int distancia = 0;
+
   //Variável recebe o valor da função da biblioteca
-  distancia = sen_dist_L.measureDistanceCm();
+  //  distancia = sen_dist_L.measureDistanceCm();
+  hcsr04_L();
+  hcsr04_R();
   Serial.print(F("L_dist: "));
   Serial.println(distancia);
-  //  distancia = sen_dist_R.measureDistanceCm();
-  //  Serial.print(F("R_dist: "));
-  //  Serial.println(distancia);
+  //  dist = sen_dist_R.measureDistanceCm();
+  Serial.print(F("R_dist: "));
+  Serial.println(dist);
+  if (dist >= distancia)
+    dist = distancia;
+
 }
 void mpu_begin() {
   Wire.begin();
@@ -114,9 +203,9 @@ void motoPassoBegin() {
   pinMode(MS3, OUTPUT);
   pinMode(STP, OUTPUT);
   pinMode(DIR, OUTPUT);
-  digitalWrite(SLP, LOW);   /* Sleep desabilitado */
+  //  digitalWrite(SLP, LOW);   /* Sleep desabilitado */
   digitalWrite(DIR, HIGH);  /* Sentido Horário habilitado */
-  digitalWrite(RST, HIGH);  /* RST habilitado */
+  //  digitalWrite(RST, HIGH);  /* RST habilitado */
 }
 void getMpuData() {
   // MPU
@@ -197,11 +286,42 @@ void ena2() {
   digitalWrite(ENA, HIGH);  /* Ativa o A4988 */
   delay(10);
 }
-
-void rst()  {      /* Reseta os drivers */
-  digitalWrite(RST, LOW);
-  delay(10);
-  digitalWrite(RST, HIGH);
-  delay(10);
-  Serial.println("Reset OK");
+void hcsr04_L() {
+  digitalWrite(TRIGGER_SL, LOW); //SETA O PINO 6 COM UM PULSO BAIXO "LOW"
+  delayMicroseconds(2); //INTERVALO DE 2 MICROSSEGUNDOS
+  digitalWrite(TRIGGER_SL, HIGH); //SETA O PINO 6 COM PULSO ALTO "HIGH"
+  delayMicroseconds(10); //INTERVALO DE 10 MICROSSEGUNDOS
+  digitalWrite(TRIGGER_SL, LOW); //SETA O PINO 6 COM PULSO BAIXO "LOW" NOVAMENTE
+  //FUNÇÃO RANGING, FAZ A CONVERSÃO DO TEMPO DE
+  //RESPOSTA DO ECHO EM CENTIMETROS, E ARMAZENA
+  //NA VARIAVEL "distancia"
+  distancia = (sen_distL.Ranging(CM)); //VARIÁVEL GLOBAL RECEBE O VALOR DA DISTÂNCIA MEDIDA
+  result = String(distancia); //VARIÁVEL GLOBAL DO TIPO STRING RECEBE A DISTÂNCIA(CONVERTIDO DE INTEIRO PARA STRING)
+  delay(5); //INTERVALO DE 500 MILISSEGUNDOS
 }
+
+void hcsr04_R() {
+  digitalWrite(TRIGGER_SR, LOW); //SETA O PINO 6 COM UM PULSO BAIXO "LOW"
+  delayMicroseconds(2); //INTERVALO DE 2 MICROSSEGUNDOS
+  digitalWrite(TRIGGER_SR, HIGH); //SETA O PINO 6 COM PULSO ALTO "HIGH"
+  delayMicroseconds(10); //INTERVALO DE 10 MICROSSEGUNDOS
+  digitalWrite(TRIGGER_SR, LOW); //SETA O PINO 6 COM PULSO BAIXO "LOW" NOVAMENTE
+  //FUNÇÃO RANGING, FAZ A CONVERSÃO DO TEMPO DE
+  //RESPOSTA DO ECHO EM CENTIMETROS, E ARMAZENA
+  //NA VARIAVEL "distancia"
+  dist = (sen_distR.Ranging(CM)); //VARIÁVEL GLOBAL RECEBE O VALOR DA DISTÂNCIA MEDIDA
+  result_2 = String(dist); //VARIÁVEL GLOBAL DO TIPO STRING RECEBE A DISTÂNCIA(CONVERTIDO DE INTEIRO PARA STRING)
+  delay(5); //INTERVALO DE 500 MILISSEGUNDOS
+}
+//void rst()  {      /* Reseta os drivers */
+//  digitalWrite(RST, LOW);
+//  delay(10);
+//  digitalWrite(RST, HIGH);
+//  delay(10);
+//  Serial.println("Reset OK");
+//}
+///////////////////////////////////////////////
+
+// distancia > 80 -> Vou para 80v
+//  80 < distancia > 100 -> Vou para 100v
+// distanci > 100 --> Vou para 155
